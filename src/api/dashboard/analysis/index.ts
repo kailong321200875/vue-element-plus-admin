3fc79c0ae7acd0929f47e33f96c8d45a90d8f762
kailong321200875 <321200875@qq.com>
2022-01-23 14:42:09 +0800

    types: delete useless types

--- src/api/dashboard/analysis/index.ts
+++ src/api/dashboard/analysis/index.ts
@@ -3,5 +3,5 @@ import { useAxios } from '@/hooks/web/useAxios'
 const { request } = useAxios()
 
 export const getCountApi = () => {
-  return request({ url: '/analysis/total', method: 'get' } as AxiosConfig)
+  return request({ url: '/analysis/total', method: 'get' })
 }
