87d307894139c7cf22357b9b5bc3b8d78d94116a
niyonggui <ni.yonggui@iwhalecloud.com>
2022-03-27 10:23:46 +0800

    chore:  remove .hbs file submission check

--- .husky/lintstagedrc.js
+++ .husky/lintstagedrc.js
@@ -4,5 +4,6 @@ module.exports = {
   'package.json': ['prettier --write'],
   '*.vue': ['prettier --write', 'stylelint --fix'],
   '*.{scss,less,styl,css,html}': ['stylelint --fix', 'prettier --write'],
-  '*.md': ['prettier --write']
+  '*.md': ['prettier --write'],
+  '*.{hbs}': ['prettier --write']
 }
