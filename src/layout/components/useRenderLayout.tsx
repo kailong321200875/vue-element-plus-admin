c295e3699ab0ac42514891ed0816765c35c7101a
muxik <lqjxm666@163.com>
2022-07-06 14:03:08 +0800

    fix: 修复template菜单样式

--- src/layout/components/useRenderLayout.tsx
+++ src/layout/components/useRenderLayout.tsx
@@ -89,7 +89,7 @@ export const useRenderLayout = () => {
               ]}
               style="transition: all var(--transition-time-02);"
             >
-              <ToolHeader class="border-bottom-1 border-solid border-[var(--top-tool-border-color)] bg-[var(--top-header-bg-color)] dark:border-[var(--el-border-color)]"></ToolHeader>
+              <ToolHeader class="border-bottom-1 border-solid border-[var(--top-tool-border-color)] dark:border-[var(--el-border-color)]"></ToolHeader>
 
               {tagsView.value ? (
                 <TagsView class="border-bottom-1 border-top-1 border-solid border-[var(--tags-view-border-color)] dark:border-[var(--el-border-color)]"></TagsView>
