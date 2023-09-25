9bcc176d165480148a119454fd60b06b78a7ea74
kailong321200875 <321200875@qq.com>
2023-09-25 15:41:35 +0800

    chore: 更新依赖

--- src/utils/propTypes.ts
+++ src/utils/propTypes.ts
@@ -1,29 +1,13 @@
-import { createTypes, VueTypesInterface, VueTypeValidableDef } from 'vue-types'
-import { CSSProperties } from 'vue'
+import VueTypes, { toType } from 'vue-types'
+import { CSSProperties, PropType } from 'vue'
 
-// 自定义扩展vue-types
-type PropTypes = VueTypesInterface & {
-  readonly style: VueTypeValidableDef<CSSProperties>
-}
-
-const propTypes = createTypes({
-  func: undefined,
-  bool: undefined,
-  string: undefined,
-  number: undefined,
-  object: undefined,
-  integer: undefined
-}) as PropTypes
-
-// 需要自定义扩展的类型
-// see: https://dwightjack.github.io/vue-types/advanced/extending-vue-types.html#the-extend-method
-propTypes.extend([
-  {
-    name: 'style',
-    getter: true,
-    type: [String, Object],
-    default: undefined
+class propTypes extends VueTypes {
+  static get style() {
+    return toType('style', {
+      type: [String, Object] as PropType<[string | CSSProperties]>,
+      default: undefined
+    })
   }
-])
+}
 
 export { propTypes }
